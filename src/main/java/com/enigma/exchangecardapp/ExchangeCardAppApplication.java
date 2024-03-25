9f14a7fa1ff62f072e650b5f9de457ee46179550
juniauni <unijunia@gmail.com>
2024-03-25 08:17:16 +0700

    [Initialization]

---
+++ src/main/java/com/enigma/exchangecardapp/ExchangeCardAppApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.exchangecardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeCardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeCardAppApplication.class, args);
+	}
+
+}
